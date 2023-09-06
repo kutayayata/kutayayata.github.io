1a82d660f9d2f857f91b418e14b77cd2a17b076b
kutayayata <osmankutayayata@gmail.com>
2023-09-06 16:30:40 +0300

    ch

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
+let player = {
+  name: "Kutay",
+  chips: 200,
+};
+
+const messageEl = document.getElementById("message_text");
+const croupierCards = document.getElementById("croupier_cards");
+const playerEl = document.getElementById("player_el");
+const sumEl = document.getElementById("sum_text");
+const yourCards = document.getElementById("your_cards");
+
 let cards = [];
 let hasBlackJack = false;
 let isAlive = false;
-let message = "";
-let messageEl = document.getElementById("message_text");
-let playerBalance = document.getElementById("player_balance")
-let playerChips = 0
 let sum = 0;
-let sumEl = document.getElementById("sum_text");
-let yourCards = document.getElementById("your_cards");
+let result = 0;
+let croupier = [];
 
 function displayGameInfo() {
   messageEl.textContent = message;
-  playerBalance.textContent = "Bakiye: ₺" + playerChips
+  const displayedCroupier = croupier.map(c => (c === 11 || c === 1) ? "A" : c);
+  croupierCards.textContent = "Krupiye: " + displayedCroupier.join(" ");
+  playerEl.textContent = player.name + ": ₺" + player.chips
   sumEl.textContent = "Toplam: " + sum;
-  yourCards.textContent = "Kartların: " + cards.join(" ");
+  const displayedCards = cards.map(card => (card === 11 || card === 1) ? "A" : card);
+  yourCards.textContent = "Kartların: " + displayedCards.join(" ");
 }
 
 function getRandomCard() {
@@ -21,7 +31,19 @@ function getRandomCard() {
 
   if (randomNumber > 10) {
     return 10;
-  } else if (randomNumber === 1) {
+  } else if (randomNumber === 1 && sum < 11) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
+}
+
+function getRandomCardCroupier() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1 && result < 11) {
     return 11;
   } else {
     return randomNumber;
@@ -29,6 +51,10 @@ function getRandomCard() {
 }
 
 function startGame() {
+  if (player.chips === 0) {
+    player.chips = 200
+  }
+  croupier = [getRandomCardCroupier()];
   cards = [getRandomCard(), getRandomCard()];
   sum = cards[0] + cards[1];
 
@@ -36,32 +62,78 @@ function startGame() {
     hasBlackJack = false;
     isAlive = true;
     message = "Bir kart daha?";
+  } else if (sum === 22) {
+    sum = 12
   } else {
     message = "Blackjack!";
+    player.chips = player.chips * 2.5;
     hasBlackJack = true;
+    croupierLimitless();
   }
 
   displayGameInfo();
 }
 
 function newCard() {
-  if (isAlive && !hasBlackJack) {
+  if (isAlive === true && sum !== 21 && hasBlackJack === false) {
     thirdCard = getRandomCard();
     sum += thirdCard;
     cards.push(thirdCard);
 
     if (sum < 21) {
+      message = "Bir kart daha?";
+    } else if (sum === 21 && result !== sum) {
+      message = "Kazandın!";
+      player.chips = player.chips * 2;
+      croupierLimitless();
+    } else {
+      message = "Patladın!";
+      player.chips = player.chips * 0;
+      croupierLimitless();
+    }
+  
+    if (sum <= 21) {
       hasBlackJack = false;
       isAlive = true;
-      message = "Bir kart daha?";
-    } else if (sum === 21) {
-      hasBlackJack = true;
-      message = "Blackjack!";
     } else {
       hasBlackJack = false;
       isAlive = false;
-      message = "Patladın!";
     }
+  
     displayGameInfo();
   }
+}
+
+function croupierLimitless() {
+  let add = (a, b) => a + b;
+  result = croupier.reduce(add);
+
+  while (result < 17) {
+    croupier.push(getRandomCardCroupier());
+    result = croupier.reduce(add);
+  }
+
+displayGameInfo();
+}
+
+function standCard() {
+  croupierLimitless();
+
+  let add = (a, b) => a + b;
+  let result = croupier.reduce(add);
+
+  if (sum <= 21 && result > 21) {
+    message = "Kazandın!"
+    player.chips = player.chips * 2;
+  } else if (result > sum) {
+    message = "Kaybettin!"
+    player.chips = player.chips * 2.5;
+  } else if (result < sum) {
+    message = "Kazandın!";
+    player.chips = player.chips * 2;
+  } else if (result = sum) {
+    message = "İade!"
+  }
+
+  displayGameInfo();
 }
